d7209f412762bc03ef54676283739f3cb3627908
riezaja <155935085+riezaja@users.noreply.github.com>
2024-07-24 13:48:02 +0700

    explore_data.py

--- explore_data.py
+++ explore_data.py
@@ -21,3 +21,38 @@ plt.title('Distribusi Durasi Tidur')
 plt.xlabel('Durasi Tidur (jam)')
 plt.ylabel('Frekuensi')
 plt.show()
+
+
+# Count plot for BMI Category
+plt.figure(figsize=(8, 6))
+sns.countplot(x='BMI Category', data=df)
+plt.title('Count Plot of BMI Category')
+plt.xlabel('Kategori BMI')
+plt.ylabel('Frekuensi')
+plt.show()
+
+# Violin plot for Stress Level by Occupation
+plt.figure(figsize=(10, 6))
+sns.violinplot(x='Occupation', y='Stress Level', data=df)
+plt.title('Violin Plot of Stress Level by Occupation')
+plt.xlabel('Pekerjaan')
+plt.ylabel('Tingkat Stres')
+plt.xticks(rotation=45)  # Rotate x-axis labels if needed
+plt.show()
+
+# Pie Chart Kategori Gangguan Tidur
+plt.figure(figsize=(8, 8))
+df['Sleep Disorder'].value_counts().plot.pie(autopct='%1.1f%%', colors=sns.color_palette('pastel'))
+plt.title('Distribusi Kategori Gangguan Tidur')
+plt.savefig('pie_chart_sleep_disorder.png')  # Simpan gambar ke file
+plt.show()  # Tampilkan gambar
+
+# Bar Plot Kualitas Tidur Berdasarkan Kategori BMI
+plt.figure(figsize=(10, 6))
+sns.barplot(x='BMI Category', y='Quality of Sleep', data=df)
+plt.title('Bar Plot Kualitas Tidur Berdasarkan Kategori BMI')
+plt.xlabel('Kategori BMI')
+plt.ylabel('Kualitas Tidur')
+plt.savefig('bar_plot_quality_of_sleep.png')  # Simpan gambar ke file
+plt.show()  # Tampilkan gambar
+
